d6131fde75faf272d047a6633fca3888156e416f
Monipulateddusk <98320488+Monipulateddusk@users.noreply.github.com>
2025-10-19 14:50:25 +0100

    Bash Testing

--- index.js
+++ index.js
@@ -1,36 +1,43 @@
 const projects = [
     {
+        title: "Perlin Noise Generator",
+        gif: "Images/ProjectImages/ProjectGifs/PerlinNoiseShowcase.gif",
+        image: "",
+        link: "#",
+        description: "This project was programmed in C++ and GLSL using OpenGL for graphics rendering. Users are able to use custom User Interface to effect the Perlin Noise image that is generated."
+        
+    },
+    {
         title: "",
+        gif: "",
         image: "",
         link: "#",
         description: ""
     },
     {
         title: "",
+        gif: "",
         image: "",
         link: "#",
         description: ""
     },
     {
         title: "",
+        gif: "",
         image: "",
         link: "#",
         description: ""
     },
     {
         title: "",
+        gif: "",
         image: "",
         link: "#",
         description: ""
     },
     {
         title: "",
-        image: "",
-        link: "#",
-        description: ""
-    },
-    {
-        title: "",
+        gif: "",
         image: "",
         link: "#",
         description: ""
@@ -42,7 +49,7 @@ const grid = document.getElementById("project-grid");
 for(const project of projects){
     const projectCard = `
         <button class="project-card" href="${project.link}">
-            <img src="${project.image}" alt="${project.title}">
+            <img src="${project.gif}" alt="${project.title}">
             <h3>${project.title}</h3>
             <p>${project.description}</p>
         </button>    
